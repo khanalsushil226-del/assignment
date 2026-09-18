2bfa9087ddb98b659ca585391bb66cdacfd48c17
khanalsushil226-del <khanalsushil226@gmail.com>
2026-09-19 01:12:07 +0545

    cleaned

--- register.js
+++ register.js
@@ -75,9 +75,10 @@ registerForm.addEventListener("submit", async (event) => {
 
         registerForm.reset();
 
-       setTimeout(() => {
-    window.location.replace("./index.html");
-}, 1500);
+        setTimeout(() => {
+            window.location.replace("./index.html");
+        }, 1500);
+
     } catch (error) {
         registerMessage.textContent = "Unable to connect to the server.";
         registerMessage.style.color = "red";
